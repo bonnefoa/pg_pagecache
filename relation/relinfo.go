a564de842cad2d452f6c9acdc4cac8eb3535431b
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2025-06-30 18:59:43 +0200

    Add relkind to RelInfo

--- relation/relinfo.go
+++ relation/relinfo.go
@@ -5,6 +5,7 @@ import "github.com/bonnefoa/pg_pagecache/pcstats"
 type RelInfo struct {
 	Relfilenode uint32
 	Relname     string
+	Relkind     rune
 	PcStats     pcstats.PcStats
 	Children    []string
 }

5ca1973b5d8af956762e770373b5cc237917530a
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2025-07-07 18:52:40 +0200

    Add missing display file

---
+++ app/display.go
@@ -0,0 +1,85 @@
+package app
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/bonnefoa/pg_pagecache/relation"
+)
+
+func (p *PgPagecache) getHeader() []string {
+	var res []string
+
+	switch p.Aggregation {
+	case AggPartition:
+		fallthrough
+	case AggPartitionOnly:
+		res = append(res, "Partition")
+		fallthrough
+	case AggTable:
+		fallthrough
+	case AggTableOnly:
+		res = append(res, "Table")
+	case AggNone: // Nothing to do
+	}
+
+	res = append(res, []string{"Relation", "Kind", "Relfilenode",
+		"PageCached", "PageCount",
+		"%Cached", "%Total"}...)
+
+	return res
+}
+
+func (p *PgPagecache) outputResults(outputInfos []relation.OutputInfo) error {
+	var values [][]string
+
+	header := p.getHeader()
+	if !p.NoHeader && p.Type != FormatJson {
+		values = append(values, header)
+	}
+
+	for _, v := range outputInfos {
+		line := v.ToStringArray(p.Unit, p.page_size, p.file_memory)
+		switch p.Aggregation {
+		case AggNone:
+			line = line[2:]
+		case AggTable:
+			fallthrough
+		case AggTableOnly:
+			line = line[1:]
+		}
+		values = append(values, line)
+	}
+
+	switch p.Type {
+	case FormatCSV:
+		w := csv.NewWriter(os.Stdout)
+		w.WriteAll(values)
+		return w.Error()
+	case FormatJson:
+		m := make([]map[string]string, 0)
+		for _, line := range values {
+			o := make(map[string]string, 0)
+			for i, k := range header {
+				o[k] = line[i]
+			}
+			m = append(m, o)
+		}
+		res, err := json.Marshal(m)
+		if err != nil {
+			return err
+		}
+		fmt.Print(string(res))
+	case FormatColumn:
+		w := tabwriter.NewWriter(os.Stdout, 14, 0, 1, ' ', 0)
+		for _, v := range values {
+			fmt.Fprintln(w, strings.Join(v, "\t"))
+		}
+		w.Flush()
+	}
+	return nil
+}
